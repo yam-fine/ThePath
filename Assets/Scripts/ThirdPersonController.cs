6a2ff4344c70bd71e734b7ba10088e0bfe22941d
yam fine <yamf45@gmail.com>
2021-12-18 22:13:30 +0200

    bugs squashed

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -47,21 +47,21 @@ namespace StarterAssets
 		[Tooltip("What layers the character uses as ground")]
 		public LayerMask GroundLayers;
 
-		[Header("Cinemachine")]
-		[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
-		public GameObject CinemachineCameraTarget;
-		[Tooltip("How far in degrees can you move the camera up")]
-		public float TopClamp = 70.0f;
-		[Tooltip("How far in degrees can you move the camera down")]
-		public float BottomClamp = -30.0f;
-		[Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
-		public float CameraAngleOverride = 0.0f;
-		[Tooltip("For locking the camera position on all axis")]
-		public bool LockCameraPosition = false;
+		//[Header("Cinemachine")]
+		//[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+		//public GameObject CinemachineCameraTarget;
+		//[Tooltip("How far in degrees can you move the camera up")]
+		//public float TopClamp = 70.0f;
+		//[Tooltip("How far in degrees can you move the camera down")]
+		//public float BottomClamp = -30.0f;
+		//[Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
+		//public float CameraAngleOverride = 0.0f;
+		//[Tooltip("For locking the camera position on all axis")]
+		//public bool LockCameraPosition = false;
 
 		// cinemachine
-		private float _cinemachineTargetYaw;
-		private float _cinemachineTargetPitch;
+		//private float _cinemachineTargetYaw;
+		//private float _cinemachineTargetPitch;
 
 		// player
 		private float _speed;
@@ -124,7 +124,7 @@ namespace StarterAssets
 
 		private void LateUpdate()
 		{
-			CameraRotation();
+			//CameraRotation();
 		}
 
 		private void AssignAnimationIDs()
@@ -149,22 +149,22 @@ namespace StarterAssets
 			}
 		}
 
-		private void CameraRotation()
-		{
-			// if there is an input and camera position is not fixed
-			if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
-			{
-				_cinemachineTargetYaw += _input.look.x * Time.deltaTime;
-				_cinemachineTargetPitch += _input.look.y * Time.deltaTime;
-			}
+		//private void CameraRotation()
+		//{
+		//	// if there is an input and camera position is not fixed
+		//	if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+		//	{
+		//		_cinemachineTargetYaw += _input.look.x * Time.deltaTime;
+		//		_cinemachineTargetPitch += _input.look.y * Time.deltaTime;
+		//	}
 
-			// clamp our rotations so our values are limited 360 degrees
-			_cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-			_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+		//	// clamp our rotations so our values are limited 360 degrees
+		//	_cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+		//	_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
 
-			// Cinemachine will follow this target
-			CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
-		}
+		//	// Cinemachine will follow this target
+		//	CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
+		//}
 
 		private void Move()
 		{
